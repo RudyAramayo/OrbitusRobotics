19555fa0271c90b3e77b3af52c4bb410276c1893
Rudy Aramayo <vzw.rudy.aramayo@outlook.com>
2018-11-26 16:33:30 -0600

    fix code

--- v/mac-pro/home/b/scripts/redirect.js
+++ v/mac-pro/home/b/scripts/redirect.js
@@ -3,7 +3,7 @@ if(!Array.prototype.indexOf){Array.prototype.indexOf=function(c){if(this==null){
 if(e!=e){e=0}else{if(e!=0&&e!=Infinity&&e!=-Infinity){e=(e>0||-1)*Math.floor(Math.abs(e))
 }}}if(e>=a){return -1}for(b=e>=0?e:Math.max(a-Math.abs(e),0);b<a;b++){if(b in d&&d[b]===c){return b
 }}return -1}}(function(h){var c,m,k,g,l;c=["ae","am","asia","at","au","befr","benl","bg","bh","br","bw","ca","ca/fr","cf","chde","chfr","ci","cm","cn","cz","de","dk","ee","eg","es","fi","fr","gn","gq","gr","gw","hk","hk/en","hr","hu","id","ie","il","in","it","jo","jp","ke","kr","kw","la","lae","li","lt","lu","lv","ma","md","me","mg","mk","ml","mt","mu","mx","my","mz","ne","ng","nl","no","nz","om","ph","pl","pt","qa","ro","ru","sa","se","sg","si","sk","sn","th","tn","tr","tw","ug","uk","vn","za"];
-k={desktop:"/mac-pro/",iPhone:"/mac-pro/index1.html"};function n(r){var p=r.split("/");
+k={desktop:"/OrbitusRobotics/mac-pro/",iPhone:"/OrbitusRobotics/mac-pro/index1.html"};function n(r){var p=r.split("/");
 var o=p[1];var q=p[2];var s=o;if(o==="ca"&&q==="fr"){s+="/"+q}else{if(o==="hk"&&q==="en"){s+="/"+q
 }}if(c.indexOf(s)>=0){return"/"+s}else{return""}}function f(o){return o.match(/AppleWebKit/i)
 }function b(o){return f(o)&&o.match(/iPad/i)}function d(o){return o.match(/iPhone/i)
